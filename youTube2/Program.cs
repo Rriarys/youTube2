c137b42293121e94bb011f151e973e25c836accc
Rriarys <rriarys@gmail.com>
2024-09-06 22:00:08 +0300

    rombus repeater

--- youTube2/Program.cs
+++ youTube2/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Diagnostics.Metrics;
+
+internal class Program
 {
     static void Main()
     {
@@ -99,7 +101,75 @@
             }
         }*/
 
+        // same, but with repeat
+        while (true)
 
+        {
+            Console.Write("Enter height of rombus (3 and more): "); int user_rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How much times repeat: "); int repeat = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int repeatsCounter = 0, repeatsInRow = 0; //счетчики общий и для строк
+
+            while (repeatsCounter < repeat)
+            {
+                int half_rows = user_rows / 2; //разбиваем ромб пополам, если 5 => будет по 2 на каждую сторону
+
+                for (int i = 1; i <= half_rows + 1; i++) //верх + 1 (центр будет рассчитан в верхней), начинаем не с 0, 
+                                                         //чтобы не получить пустую строку в первом ряду
+                {
+                    for (int sp = 0; sp < repeatsInRow; sp++) // sp - spaces
+                    {
+                        Console.Write("   "); //3 пробелов
+                    }
+
+                    for (int j = i; j <= half_rows; j++) //пробелы, сравниваются с половиной, на последней строке нет отступов
+                    {
+                        Console.Write(' ');
+                    }
+
+                    for (int k = 1; k <= (2 * i - 1); k++) //звезды, должна быть хоть 1 звезда; 
+                    {                                      //каждую центральную обрамляет 2 дополнительные звезды
+                                                           //  x     1 позиция -> всегда 1
+                        Console.Write("*");                // xxx  (x) 2 позиция 2 * 2 -> 4, должно получатся нечетное число, чтобы сохранить центрирование по вертикальной оси
+                                                           //xxxxx (x) 3 позиция 3 * 2 -> 6, прослеживается закономерность
+                    }
+                    
+                    Console.WriteLine();
+                }
+
+                for (int i = half_rows; i >= 1; i--) //тоже самое, только точка отсчета это середина
+                {
+                    for (int sp = 0; sp < repeatsInRow; sp++) // sp - spaces
+                    {
+                        Console.Write("   "); //3 пробелов
+                    }
+
+                    for (int j = half_rows; j >= i; j--)
+                    {
+                        Console.Write(' ');
+                    }
+
+                    for (int k = 1; k <= (2 * i - 1); k++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+                repeatsCounter++;
+                repeatsInRow++;
+
+                if (repeatsInRow == 5) // 5 в каждую строку
+                {
+                    Console.WriteLine();
+                    repeatsInRow = 0;
+                }
+
+            }
+        }
 
 
 
